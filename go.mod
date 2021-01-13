84fec25c708c9be067805a9f41e9bc1ee018b57a
choopong <cchoopong@gmail.com>
2021-01-13 12:45:25 +0700

    initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/shall-we-go/someproject
+
+go 1.15
+
+require github.com/shall-we-go/somemodule v1.2.0
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/shall-we-go/somemodule"
+)
+
+func main() {
+	fmt.Println(somemodule.HelloWithVersion())
+}
